50994414419bebecdc959b7ba46f69651234e4be
wenghengcong <wenghengcong@gmail.com>
2018-08-18 09:41:53 +0800

    add download pop

---
+++ BeeFunMac/BeeFunMac/View/Star/View/BFStarDownloadController.swift
@@ -0,0 +1,18 @@
+//
+//  BFStarDownloadController.swift
+//  BeeFun
+//
+//  Created by WengHengcong on 2018/8/18.
+//  Copyright © 2018年 LuCi. All rights reserved.
+//
+
+import Cocoa
+
+class BFStarDownloadController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
