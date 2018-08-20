1d8b7956f42b72e7cab75d4d7959cd99be559794
wenghengcong <wenghengcong@gmail.com>
2018-08-21 07:42:20 +0800

    修改颜色

--- BeeFunMac/BeeFunMac/View/Star/View/BFStarDownloadController.swift
+++ BeeFunMac/BeeFunMac/View/Star/View/BFStarDownloadController.swift
@@ -10,9 +10,51 @@ import Cocoa
 
 class BFStarDownloadController: NSViewController {
 
+    @IBOutlet weak var urlStackView: NSStackView!
+    
+    @IBOutlet weak var sshBackView: BFView!
+    @IBOutlet weak var sshTitleLabel: NSTextField!
+    @IBOutlet weak var sshContentLabel: NSButton!
+    @IBOutlet weak var sshImageBtn: NSButton!
+    
+    @IBOutlet weak var httpsBackView: NSView!
+    @IBOutlet weak var httpsTitleLabel: NSTextField!
+    @IBOutlet weak var httpsContentLabel: NSButton!
+    @IBOutlet weak var httpsImageBtn: NSButton!
+    
+    @IBOutlet weak var openBackView: NSView!
+    @IBOutlet weak var openXcodeBtn: NSButton!
+    @IBOutlet weak var openDesktopBtn: NSButton!
+    
+    @IBOutlet weak var downloadZipBackView: NSView!
+    @IBOutlet weak var downloadZipBtn: NSButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        customView()
+    }
+    
+    func customView() {
+        urlStackView.backgroundColor = NSColor.thDayWhite
+        sshBackView.backgroundColor = NSColor.thDayWhite
+        httpsBackView.backgroundColor = NSColor.thDayWhite
+        openBackView.backgroundColor = NSColor.thDayWhite
+        downloadZipBackView.backgroundColor = NSColor.thDayWhite
+
+        sshTitleLabel.backgroundColor = NSColor.thDayWhite
+        sshContentLabel.backgroundColor = NSColor.thDayWhite
+        sshImageBtn.backgroundColor = NSColor.thDayWhite
+        
+        httpsTitleLabel.backgroundColor = NSColor.thDayWhite
+        httpsContentLabel.backgroundColor = NSColor.thDayWhite
+        httpsImageBtn.backgroundColor = NSColor.thDayWhite
+        
+        openXcodeBtn.backgroundColor = NSColor.clear
+        openDesktopBtn.backgroundColor = NSColor.clear
+        downloadZipBtn.backgroundColor = NSColor.clear
+        
     }
     
 }
