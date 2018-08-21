1a86ad01e2774d08dfc72d8311b9caa894c28638
wenghengcong <wenghengcong@gmail.com>
2018-08-21 22:05:02 +0800

    UI视觉完成

--- BeeFunMac/BeeFunMac/View/Star/View/BFStarDownloadController.swift
+++ BeeFunMac/BeeFunMac/View/Star/View/BFStarDownloadController.swift
@@ -43,6 +43,9 @@ class BFStarDownloadController: NSViewController {
     }
     
     func customView() {
+        
+        view.backgroundColor = NSColor.thDayWhite
+        
         urlStackView.backgroundColor = NSColor.thDayWhite
         sshBackView.backgroundColor = NSColor.thDayWhite
         httpsBackView.backgroundColor = NSColor.thDayWhite
@@ -51,16 +54,19 @@ class BFStarDownloadController: NSViewController {
 
         sshTitleLabel.backgroundColor = NSColor.thDayWhite
         sshContentLabel.backgroundColor = NSColor.thDayWhite
+        sshContentLabel.borderWidth = 1.0
+        sshContentLabel.borderColor = NSColor.thDayLightGray
         sshImageBtn.backgroundColor = NSColor.thDayWhite
         
         httpsTitleLabel.backgroundColor = NSColor.thDayWhite
         httpsContentLabel.backgroundColor = NSColor.thDayWhite
+        httpsContentLabel.borderWidth = 1.0
+        httpsContentLabel.borderColor = NSColor.thDayLightGray
         httpsImageBtn.backgroundColor = NSColor.thDayWhite
         
         openXcodeBtn.backgroundColor = NSColor.thDayWhite
         openDesktopBtn.backgroundColor = NSColor.thDayWhite
         downloadZipBtn.backgroundColor = NSColor.thDayWhite
-        
         
     }
     
@@ -71,15 +77,15 @@ class BFStarDownloadController: NSViewController {
             pstyle.alignment = .left
             let titleDic = [NSAttributedStringKey.foregroundColor : NSColor.thDayBlack, NSAttributedStringKey.paragraphStyle : pstyle, NSAttributedStringKey.backgroundColor: NSColor.thDayWhite, NSAttributedStringKey.font: NSFont.bfSystemFont(ofSize: 12.0)] as [NSAttributedStringKey : Any]
             
-            let urlConentDic = [NSAttributedStringKey.foregroundColor : NSColor.thDayWhite, NSAttributedStringKey.paragraphStyle : pstyle, NSAttributedStringKey.backgroundColor: NSColor.thDayBlue, NSAttributedStringKey.font: NSFont.bfSystemFont(ofSize: 12.0)] as [NSAttributedStringKey : Any]
+            let urlConentDic = [NSAttributedStringKey.foregroundColor : NSColor.thDayBlack, NSAttributedStringKey.paragraphStyle : pstyle, NSAttributedStringKey.backgroundColor: NSColor.thDayWhite, NSAttributedStringKey.font: NSFont.bfSystemFont(ofSize: 12.0)] as [NSAttributedStringKey : Any]
             
             let contentDic = [NSAttributedStringKey.foregroundColor : NSColor.thDayBlack, NSAttributedStringKey.paragraphStyle : pstyle, NSAttributedStringKey.backgroundColor: NSColor.thDayWhite, NSAttributedStringKey.font: NSFont.bfSystemFont(ofSize: 13.0)] as [NSAttributedStringKey : Any]
             
             sshTitleLabel.attributedStringValue = NSAttributedString(string: "Clone with SSH", attributes: titleDic)
-            sshContentLabel.attributedTitle = NSAttributedString(string: objrepo.ssh_url!, attributes: urlConentDic)
+            sshContentLabel.attributedTitle = NSAttributedString(string: " " + objrepo.ssh_url!, attributes: urlConentDic)
             
             httpsTitleLabel.attributedStringValue = NSAttributedString(string: "Clone with HTTPS", attributes: titleDic)
-            httpsContentLabel.attributedTitle = NSAttributedString(string: objrepo.clone_url!, attributes: urlConentDic)
+            httpsContentLabel.attributedTitle = NSAttributedString(string:" " + objrepo.clone_url!, attributes: urlConentDic)
             
             openXcodeBtn.attributedTitle = NSAttributedString(string: "  Open in Xcode", attributes: contentDic)
             openDesktopBtn.attributedTitle = NSAttributedString(string: "  Open in Desktop", attributes: contentDic)
