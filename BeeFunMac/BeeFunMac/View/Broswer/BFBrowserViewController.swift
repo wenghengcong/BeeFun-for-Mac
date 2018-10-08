4c7dad23b48093ca0944bb69befd28c04125a28a
翁恒丛 <wenghengcong@gmail.com>
2018-10-08 19:13:09 +0800

    web视图

--- BeeFunMac/BeeFunMac/View/Broswer/BFBrowserViewController.swift
+++ BeeFunMac/BeeFunMac/View/Broswer/BFBrowserViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import Cocoa
+import WebKit
 
 class BFBrowserViewController: NSViewController {
 
+    @IBOutlet weak var navigationContainView: NSView!
+    
+    @IBOutlet weak var backButton: NSButton!
+    @IBOutlet weak var forwardButton: NSButton!
+    
+    @IBOutlet weak var refreshButton: NSButton!
+    
+    @IBOutlet weak var websiteInputContainView: NSView!
+    
+    @IBOutlet weak var websiteInputTextField: NSTextField!
+    
+    @IBOutlet weak var webViewContainView: NSView!
+    @IBOutlet weak var webprogressBar: NSProgressIndicator!
+    @IBOutlet weak var websiteView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        navigationContainView.backgColor = NSColor.white
+        webViewContainView.backgColor = NSColor.white
+        
+        webprogressBar.alphaValue = 0.0
+        webprogressBar.isHidden = true
     }
     
 }
